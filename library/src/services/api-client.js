78d7a7af93ae5518e1bc056a3cbef0e5d5e5c374
Etika <96071949+Etika8445@users.noreply.github.com>
2024-06-05 22:32:28 +0530

    Update api

--- library/src/services/api-client.js
+++ library/src/services/api-client.js
@@ -2,7 +2,7 @@ import axios from "axios";
 async function getdata({titlename}){
     const URL=`https://openlibrary.org/search.json?q=${titlename}&limit=10&page=1`;
     const response= await axios.get(URL);
-    console.log(response.data.docs);
+    // console.log(response.data.docs);
     return response.data.docs;
 }
 export {getdata};
